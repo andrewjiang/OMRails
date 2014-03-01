4d9ea25ed70b44fa7d342eae4eb0591d39af4b77
Andrew Jiang <andrew.h.jiang@gmail.com>
2014-03-01 12:46:22 -0500

    Add jQuery Masonry

--- app/assets/javascripts/pins.js
+++ app/assets/javascripts/pins.js
@@ -1,2 +1,9 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+var container = document.querySelector('#pins');
+var msnry = new Masonry( container, {
+  // options...
+  itemSelector: '.box',
+  columnWidth: 200
+});
